e80a91590d73c5c53bf0382e4467321830e50fd8
Pierre-Antoine Rouby <pa.rouby@gmail.com>
2018-02-13 08:51:03 +0100

    add php and create html template

--- www/index.php
+++ www/index.php
@@ -23,49 +23,7 @@
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
     <![endif]-->
 
-    <header class="section">
-      <section class="hero is-warning">
-        <!-- Hero head: will stick at the top -->
-        <div class="hero-head">
-          <nav class="navbar">
-            <div class="container">
-              <div class="navbar-brand">
-                <a class="navbar-item">
-                  <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
-                </a>
-                <span class="navbar-burger burger" data-target="navbarMenuHeroA">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </span>
-              </div>
-              <div id="navbarMenuHeroA" class="navbar-menu">
-                <div class="navbar-end">
-                  <a class="navbar-item is-active" href="index.html">
-                    Accueil
-                  </a>
-                  <a class="navbar-item" href="recette.html">
-                    Recettes
-                  </a>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-
-        <!-- Hero content: will be in the middle -->
-        <div class="hero-body">
-          <div class="container has-text-centered">
-            <h1 class="title">
-              Marmitonne
-            </h1>
-            <h2 class="subtitle">
-              La cuisine qui détonne
-            </h2>
-          </div>
-        </div>
-      </section>
-    </header>
+    <?php include ("template/header.html"); ?>
 
     <section class="section">
       <div class="container">
@@ -137,18 +95,7 @@
       </div>
     </section>
 
-    <footer class="footer">
-      <div class="container">
-        <div class="content has-text-centered">
-          <p>
-            <strong>ADSILLH Bulma</strong> by Rhenaud Dubois,
-            <a href="https://parouby.fr">Pierre-Antoine Rouby</a>.
-            The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-          </p>
-        </div>
-      </div>
-    </footer>
+    <?php include ("template/footer.html"); ?>
 
     <script src="js/vendor/modernizr-3.5.0.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
